42f400c5fce11e342f205fd2ef622096746763ef
Idiotischer <idiotischerderechte@gmail.com>
2025-08-21 21:59:41 +0200

    idk der wievielte commit das ist, hab lange nix commited

--- src/main/java/xyz/blackdev/utilityxcore/config/ConfigManager.java
+++ src/main/java/xyz/blackdev/utilityxcore/config/ConfigManager.java
@@ -1,8 +1,37 @@
 package xyz.blackdev.utilityxcore.config;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import de.craftsblock.craftscore.json.Json;
+import de.craftsblock.craftscore.json.JsonParser;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class ConfigManager {
 
-    public boolean createConfig() {
-        return true;
+    public Json createConfig(String name, Path dir) {
+        return createConfig(dir.resolve(name), dir);
+    }
+
+    public Json createConfig(Path file, Path dir) {
+        if (Files.notExists(dir)) {
+            try {
+                Files.createDirectories(dir);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if(Files.notExists(file)) {
+            try {
+                Files.createFile(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return JsonParser.parse(file);
     }
 }
